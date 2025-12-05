afdd3c301e4da60ea568930e439f1a478bc61ad4
Gerardo Gallegos, Jr. <63253771+GeraG18@users.noreply.github.com>
2025-12-04 22:39:48 -0600

    frsefesf

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), alpinejs()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
